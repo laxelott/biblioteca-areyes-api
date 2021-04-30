0c6853c710414fde92a30d6dd95c9de28a0f8792
Laxelott <laxelott@hotmail.com>
2021-04-29 19:03:50 -0500

    Agregar html

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import Hapi from "@hapi/hapi";
 import DB from "./db.js";
+import path from "path";
 
 const server = Hapi.server({
 	port: 3500,
 	host: "localhost",
+});
+
+await server.register(require("@hapi/vision"));
+
+server.views({
+	engines: {
+		html: require("handlebars"),
+	},
+	relativeTo: __dirname,
+	path: "templates",
+});
+
+// ROUTES
+server.route({
+	method: "GET",
+	path: "/",
+	handler: async (request, reply) => {
+		reply.file(path.join(path.resolve(path.dirname("")), "holi.html"));
+	},
 });
 
 server.route({
